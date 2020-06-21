2673166d7a7c2534f6f6c2a1da164dfa638bb4c4
Maksym Izmailov <maksym.izmailov.lv@gmail.com>
2020-06-21 14:14:52 +0300

    Database Connectivity

---
+++ database/config.ts
@@ -0,0 +1,22 @@
+import { MongoClient } from 'https://deno.land/x/mongo@v0.8.0/mod.ts';
+import { URI, dbName } from '../keys/appConstants.ts';
+
+class Database {
+    /**
+     *  Database Connectivity
+     */
+
+    //Fields
+    client: any;
+    db: any;
+    collection: any;
+
+    constructor(collectionName: string) {
+        const client = new MongoClient();
+        client.connectWithUri(URI);
+        this.db = client.database(dbName);
+        this.collection = this.db.collection(collectionName);
+    }
+}
+
+export default Database;
