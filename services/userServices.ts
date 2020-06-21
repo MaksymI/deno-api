626f7fdf0e2c3b0fbb50ef0b980f41a2d0033290
Maksym Izmailov <maksym.izmailov.lv@gmail.com>
2020-06-21 14:11:04 +0300

    userServices

---
+++ services/userServices.ts
@@ -0,0 +1,15 @@
+import { getUserById, createUser } from './../repository/userDAL.ts';
+
+export const createUserService = async (body: any) => {
+    let newUser = await createUser(body);
+    return newUser;
+};
+
+export const getUserService = async (params: any) => {
+    try {
+        let userData = await getUserById(params.id);
+        return userData;
+    } catch (e) {
+        throw e;
+    }
+};
