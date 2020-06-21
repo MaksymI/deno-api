831b1f078a538f61c744c4b9a36e71bb2ba7a6b0
Maksym Izmailov <maksym.izmailov.lv@gmail.com>
2020-06-21 14:14:35 +0300

    user DAL

---
+++ repository/userDAL.ts
@@ -0,0 +1,15 @@
+import Database from '../database/config.ts';
+const User: any = new Database('user');
+
+export const createUser = async (data: any) => {
+    let result: any = await User.collection.insertOne(data);
+    return result;
+};
+
+export const getUserById = async (query: any) => {
+    try {
+        return await User.collection.find();
+    } catch (e) {
+        throw e;
+    }
+};
